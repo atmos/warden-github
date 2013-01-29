c485480798b1e878389969a46847dda4a4e553c5
Philipe Fatio <philipe.fatio@gmail.com>
2013-01-29 19:47:21 +0100

    Remove unused error class

--- lib/warden/github.rb
+++ lib/warden/github.rb
@@ -2,12 +2,6 @@ require 'warden'
 require 'oauth2'
 require 'yajl'
 
-module Warden
-  module GitHub
-    class GitHubMisconfiguredError < StandardError; end
-  end
-end
-
 require 'warden/github/user'
 require 'warden/github/proxy'
 require 'warden/github/version'
